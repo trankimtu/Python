c5d90cc02fc9f90ea19ac0720ae6ab04c64a1393
trankimtu <33793431+trankimtu@users.noreply.github.com>
2020-04-17 10:34:09 -0700

    Add files via upload

--- compiler/syntaxer/syntaxer.py
+++ compiler/syntaxer/syntaxer.py
@@ -5,6 +5,9 @@ class Syntaxer (object):
 
     # statemenize method - create statement list from lexemes
     def syntaxer(self):
+
+
+
         lexemes = list(self.lexemes[0])
         # lexemes = [subject 
         #            for lexeme in self.lexemes
@@ -38,8 +41,18 @@ class Syntaxer (object):
         # print('begin before while = ', begin)
 
 
+        print(f'begin = {begin}')
+
+        isStatement, result, newBegin = Statement(lexemes, begin)
+
+        print(f'isStatement = {isStatement}')
+        print(f'result = {result}')
+        print(f'newBegin = {newBegin}')
+        # Statement(lexemes, begin)
+
 
         print('len(lexemes) = ', len(lexemes))
+
 
 
         
@@ -52,7 +65,7 @@ class Syntaxer (object):
             print('Result = ', result)
            
             begin = newBegin
-            print('\n\n')
+            print('\n')
 
 
         print('after while')
@@ -62,13 +75,195 @@ class Syntaxer (object):
 # End class here
 # ==============================================
 
+def Statement(arg, begin):
+    print (f'begin in Statement = {begin}')
+    IsAssign, AssignResult, AssignBegin = isAssign(arg, begin)
+    IsDeclarative, DeclarativeResult, DeclarativeBegin = isDeclarative(arg, begin)
+    # isIf(arg, begin)
+    # isWhile(arg, begin)
+    # isBegin(arg, begin)
+    if IsAssign == True:
+        IsStatement, stamentResult, statementBegin = IsAssign, result, newBegin
+    elif IsDeclarative == True:
+        IsStatement, stamentResult, statementBegin = IsDeclarative, DeclarativeResult, DeclarativeBegin
+    else:
+        print('Not a statement')
+
+
+    return IsStatement, stamentResult, statementBegin
+
+def isAssign(arg, begin):
+
+    IsID, idKey, idValue = isID(arg[begin])
+    if IsID == True:
+        opreatorKey, operatorValue = getKeyValue(arg[begin + 1])
+        if operatorValue == '=':
+            isExpression[begin + 2] =========================================================]
+
+
+def isDeclarative(arg, begin):
+    IsType, typeKey, typeValue = isType(arg[begin])
+    IsID, idKey, idValue = isID(arg[begin + 1])
+    IsSemicolon = isSemicolon(arg[begin + 2])
+
+    if IsType & IsID & IsSemicolon == True:
+
+        result = {
+            'Token': 'Declaration',
+            # 'Lexeme': '',
+            'BNF': '<Statement> -> <Declarative> \n <Declarative> -> <Type> <id> ;'
+        }
+        newBegin = begin + 3
+        return True, result, newBegin
+    else: 
+        return False, -1, begin
+
+    
+def isIf(arg, begin):
+    pass
+def isWhile(arg, begin):
+    pass
+def isBegin(arg, begin):
+    pass
+
+def getKeyValue (mydict):
+    # print('dict = ', mydict)
+    for key, value in mydict.items():
+        return key, value
+
+
+def isType(typeDict):
+
+    # typeKey = list(typeDic.keys())[0]
+    # typeValue = list(typeDic.values())[0]
+
+    # print(typeKey)
+    # print(typeValue)
+
+    typeKey, typeValue = getKeyValue (typeDict)
+
+    typeList = ['int', 'float', 'bool']
+
+    if typeValue in typeList:
+        return True, typeKey, typeValue
+    else:
+        return False, 'Not Type Key', 'Not Type Value'
+
+
+def isRelop (relopDict):
+    relopKey, relopValue = getKeyValue(relopDict)
+
+    relopList = ['<', '<=', '==', '<>', '>=', '>' ]
+
+    if relopValue in relopList:
+        return True, relopValue
+    else:
+        return False, 'Not a relop'
+
+def isExpression(*arg, begin):
+    pass
+
+def isTerm(*arg, begin):
+    pass
+
+
+def isFactor(*arg, begin):
+    
+    IsID, IdKey, IdValue = isID(arg[begin])
+    if IsID == True:
+        return True, IdKey, IdValue
+
+    IsNum, numKey, numValue = isNum(arg[begin])
+    if IsNum == True:
+        return True, numKey, numValue
+
+    
+    # openParenthesisKey, openParenthesisValue = getKeyValue(arg[begin])
+    # if openParenthesisValue = '(':
+    #     IsExpression, expressionEnd = isExpression (arg, begin + 1)
+
+
+
+
+    if len(arg) == 0:
+        print("Error, Factor must be provided!")
+        return False
+    
+    if len(arg) == 1:
+        print(arg[0].keys())
+
+
+def isID (idDict):
+    # idKey = list(idDic.keys())[0]
+    # idValue = list(idDic.values())[0]
+
+    # print(idKey)
+    # print(idValue)
+
+    idKey, idValue = getKeyValue(idDict)
+
+    if idKey == 'IDENTIFIER':
+        return True, idKey, idValue
+    else:
+        return False, 'Not ID Key', 'Not ID Value'
+
+def isNum(numDict):
+
+    # numKey = list(numDic.keys())[0]
+    # numValue = list(numDic.values())[0]
+
+    # print(numKey)
+    # print(numValue)
+
+    numKey, numValue = getKeyValue(numDict)
+
+    numType = ['FLOAT', 'INT']
+
+    if numKey in numType:
+        return True, numKey, numValue
+    else:
+        return False, 'NAN Key', 'NAN Value' 
+    
+def isSemicolon(semicolonDict):
+    semicolonKey, semicolonValue = getKeyValue(semicolonDict)
+    # semicolonValue = list(semicolonDic.values())[0]
+
+    # print(numKey)
+    # print(numValue)
+
+    if semicolonValue == ';':
+        return True
+    else:
+        return FalsesemicolonValue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ========================================================================================
+# ignore those stuff bellow
+# ========================================================================================
+
+
 def checkAllRules(arg, begin):
     # print('My arg = ', arg)
     # print('begin in checkAllRules = ', begin)
     # print('len(arg) in checkAllRules = ', len(arg))
 
     availableLen = len(arg) - begin
-    print('availableLen = ', availableLen)
+    # print('availableLen = ', availableLen)
 
     # Check declaration : int a; len = 3 --------------
     if availableLen >= 3:
@@ -126,10 +321,6 @@ def checkAllRules(arg, begin):
 
 
 
-def getKeyValue (mydict):
-    # print('dict = ', mydict)
-    for key, value in mydict.items():
-        return key, value
 
 
 # <Declaration> -> <Type> <IDs>;        Example : int a
@@ -139,7 +330,7 @@ def isDeclaration (arg, begin):
     # print('arg in isDeclaration = ')
     # for i in arg:
     #     print(f'{i}')
-    print()
+    # print()
 
 
     # print(f'arg[{begin}] = ', arg[begin])
@@ -175,8 +366,8 @@ def isDeclaration (arg, begin):
 # <A1> -> <ID> = <NUM> ;      a = 1
 # <Define> -> <IDs> = <NUM> ;       Example: a = 1
 def isDefine1 (arg, begin):
-    print()
-    print('begin in isDefine1 = ', begin)
+    # print()
+    # print('begin in isDefine1 = ', begin)
     # print('arg in isDefine1 = ')
     # for i in arg:
     #     print(f'{i}')
@@ -232,8 +423,14 @@ def isDefine1 (arg, begin):
 
 # <A2> -> <ID> = <ID> ;       a = b
 def isDefine2 (arg, begin):
+# B
+# 1
+# n
+# h
+# Tr 4 n 
 
-    print()
+
+    # print()
     # print('arg in isDefine2 = ')
     # for i in arg:
     #     print(f'{i}')
